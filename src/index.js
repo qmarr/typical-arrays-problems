0aa40d8a92abd0fc555f828b54822dbf1320aee3
qmarr <komarenetsv@gmail.com>
2021-02-23 13:17:21 +0200

    feat: solve the problem with arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments[0] == undefined) return 0;
+    if(array.length == 0) {
+  		return array.length
+  	}
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments[0] == undefined) return 0;
+    if(array.length == 0) {
+  		return array.length
+  	}
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if(arguments[0] == undefined) return 0;
+    if(array.length == 0) {
+  		return array.length
+  	}
+  for(let arg of array) {
+    sum+=arg;
+  }
+  return sum/(array.length);
 }
